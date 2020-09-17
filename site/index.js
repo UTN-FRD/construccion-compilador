41ffe15f4b89232d28465d861854a4bca37d81a5
Juan Bono <juanbono94@gmail.com>
2020-09-17 18:26:06 -0300

    add extra code

--- site/index.js
+++ site/index.js
@@ -1,14 +1,17 @@
 import * as lisp from "frd_lisp";
 
 let input = document.getElementById("repl-input");
+
+let extracode = document.getElementById("repl-extra-code");
 let output = document.getElementById("repl-output");
 
 input.addEventListener("keyup", event => {
     if (event.code === "Enter") {
         event.preventDefault();
         let content = input.value;
-
-        let parsed_value = new lisp.LispVal(content);
+        let extra = extracode.value;
+        console.log(extra + content);
+        let parsed_value = new lisp.LispVal(extra + content);
 
         output.innerHTML = parsed_value.toString();
 
