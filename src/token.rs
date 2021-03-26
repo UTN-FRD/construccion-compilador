97626ba9ecbe347240766b9e0a924cb054fa771a
Juan Bono <juanbono94@gmail.com>
2021-03-26 09:49:13 -0300

    fix identifier lexing

--- src/token.rs
+++ src/token.rs
@@ -12,6 +12,7 @@ pub enum Token<'input> {
     Comma,
     Define,
     If,
+    Identifier(&'input str),
     String(&'input str),
 }
 
@@ -45,6 +46,14 @@ pub fn tokenize<'input>(s: &'input str) -> Vec<(usize, Token<'input>, usize)> {
                     tokens.push(Token::Num(f64::from_str(&s[ci..slice_end]).unwrap()));
                     continue;
                 }
+                _ if c.is_alphanumeric() => {
+                    let (slice_end, new_lookahead) = take_while(ci, &mut char_indices, |c| {
+                        !c.is_whitespace() && c != '(' && c != ')'
+                    });
+                    lookahead = new_lookahead;
+                    tokens.push(parse_identifier(&s[ci..slice_end]));
+                    continue;
+                }
                 _ => {
                     panic!("invalid character: {:?}", c);
                 }
@@ -60,6 +69,14 @@ pub fn tokenize<'input>(s: &'input str) -> Vec<(usize, Token<'input>, usize)> {
         .enumerate()
         .map(|(i, tok)| (i * 2, tok, i * 2 + 1))
         .collect()
+}
+
+fn parse_identifier(s: &str) -> Token {
+    match s {
+        "define" => Token::Define,
+        "if" => Token::If,
+        ident => Token::Identifier(ident),
+    }
 }
 
 fn take_while<F>(
@@ -125,18 +142,49 @@ fn tok5_test() {
     assert_eq!(tokens, [(0, Token::String("another string"), 1)])
 }
 
-
 #[test]
 fn tok6_test() {
     let source_code = "(define (id x) (x))";
     let tokens = tokenize(source_code);
-    assert_eq!(tokens, [(0, Token::String("another string"), 1)])
+    assert_eq!(
+        tokens,
+        [
+            (0, Token::LParen, 1),
+            (2, Token::Define, 3),
+            (4, Token::LParen, 5),
+            (6, Token::Identifier("id"), 7),
+            (8, Token::Identifier("x"), 9),
+            (10, Token::RParen, 11),
+            (12, Token::LParen, 13),
+            (14, Token::Identifier("x"), 15),
+            (16, Token::RParen, 17),
+            (18, Token::RParen, 19)
+        ]
+    )
 }
-
 
 #[test]
 fn tok7_test() {
     let source_code = "\"hello";
     let tokens = tokenize(source_code);
     assert_eq!(tokens, [(0, Token::String("another string"), 1)])
+}
+
+#[test]
+fn tok8_test() {
+    let source_code = "(define (k x) 12345)";
+    let tokens = tokenize(source_code);
+    assert_eq!(
+        tokens,
+        [
+            (0, Token::LParen, 1),
+            (2, Token::Define, 3),
+            (4, Token::LParen, 5),
+            (6, Token::Identifier("k"), 7),
+            (8, Token::Identifier("x"), 9),
+            (10, Token::RParen, 11),
+            (12, Token::Num(12345.0), 13),
+            (14, Token::RParen, 15)
+        ]
+    )
 }
