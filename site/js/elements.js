6346a7c78125a9f126de58bd33d923ff49990fe5
Sergio Viera <sergioviera@gmail.com>
2021-03-30 18:25:03 -0300

    improve console

--- site/js/elements.js
+++ site/js/elements.js
@@ -1,6 +1,5 @@
 export const elements = {
   replInput: document.getElementById("repl-input"),
-  replDefinitions: document.getElementById("repl-extra-code"),
   output: document.getElementById("repl-output"),
   execute: document.getElementById("execute"),
 };
--- site/js/index.js
+++ site/js/index.js
@@ -1,29 +1,34 @@
 import * as lisp from "frd_lisp";
 import { elements } from "./elements";
 
-const codeCodeMirror = CodeMirror.fromTextArea(elements.replDefinitions, {
-  lineNumbers: true,
-  mode: "haskell",
-});
 const shell = CodeMirror.fromTextArea(elements.replInput, {
   lineNumbers: true,
   theme: "ayu-dark",
   mode: "shell",
-});
-shell.setOption("theme", "ayu-dark");
-
-elements.execute.addEventListener("click", () => {
-  try {
-    // get shell input
-    const shellValue = shell.getValue();
-
-    // parse shell input
-    const parsed_value = new lisp.LispVal(shellValue);
-    elements.output.innerHTML = parsed_value.toString();
-
-    // clear shell value
-    shell.setValue("");
-  } catch (e) {
-    elements.output.innerHTML = new Date() + " ERROR - " + e;
+  extraKeys: {
+    "Enter": onNewLine
   }
 });
+
+shell.setOption("theme", "ayu-dark");
+
+
+function onNewLine(e){
+  // get shell input
+  const shellValue = shell.getValue();
+
+  shell.replaceSelection("\n" ,"end");
+  var doc = shell.getDoc();
+  var cursor = doc.getCursor();
+
+  try {
+    // parse shell input
+    const parsed_value = new lisp.LispVal(shellValue);
+    const result = parsed_value.toString();
+
+    doc.replaceRange(result+"\n", cursor);
+  } catch (e) {
+    doc.replaceRange("ERROR > "+e+"\n", cursor);
+  }
+
+}
