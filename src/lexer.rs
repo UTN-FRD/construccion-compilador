515d6a728bdc0fa1d347730b998badef1da2e191
Juan Bono <juanbono94@gmail.com>
2021-06-24 10:16:15 -0300

    add some tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,47 +1,99 @@
 use logos::Logos;
-use std::str::FromStr;
+
 #[derive(Debug, Copy, Clone, PartialEq, Logos)]
 enum Token<'input> {
-	#[regex("(\\-)?[0-9]+(\\.[0-9]+)?", |t| f64::from_str(t.slice()))]
-	Num(f64),
-	#[token("(")]
+    #[regex("-?[0-9]+\\.[0-9]+", |t| t.slice().parse())]
+    Num(f64),
+    #[token("(")]
     LParen,
-	#[token(")")]
+    #[token(")")]
     RParen,
-	#[token("-")]
+    #[token("-")]
     Minus,
-	#[token("+")]
+    #[token("+")]
     Plus,
-	#[token("*")]
+    #[token("*")]
     Times,
-	#[token("/")]
+    #[token("/")]
     Div,
-	#[token("=")]
+    #[token("=")]
     Equal,
-	#[token(">")]
+    #[token(">")]
     GreaterThan,
-	#[token("<")]
+    #[token("<")]
     LessThan,
-	#[token(",")]
+    #[token(",")]
     Comma,
-	#[token("define")]
+    #[token("define")]
     Define,
-	#[token("if")]
+    #[token("if")]
     If,
-	#[regex("[A-Za-z][A-Za-z0-9]+")]
+    #[regex("[A-Za-z][A-Za-z0-9]+")]
     Identifier(&'input str),
-	#[regex("\"[a-zA-Z]+\"", |t| {let s = t.slice(); &s[1..(s.len() - 1)]})]
+    #[regex("\"[a-zA-Z]+\"", |t| {let s = t.slice(); &s[1..(s.len() - 1)]})]
     String(&'input str),
-	#[error]
-	Error,
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    #[error]
+    Error,
 }
 
+struct Span {}
 
+pub struct TokenInfo<'input> {
+	token: Token<'input>,
+	pos: Span,
+	text: String,
+}
+
+// TODO: Hacer una funcion que en vez de producir Tokens, produzca 
+// un vector de TokenInfo
+pub fn tokenize<'input>(source: &str) -> Vec<TokenInfo<'input>> {
+	let tokens: Vec<TokenInfo> = vec![];
+	let mut lex = Token::lexer(source);
+	//
+	unimplemented!()
+}
 
 #[test]
 fn can_parse_strings() {
-	let mut lex = Token::lexer("\"hello\"");
+    let mut lex = Token::lexer("\"hello\"");
 
-	assert_eq!(lex.next(), Some(Token::String("hello")));
+    assert_eq!(lex.next(), Some(Token::String("hello")));
     assert_eq!(lex.slice(), "\"hello\"");
 }
+
+#[test]
+fn can_parse_float_numbers() {
+    let mut lex = Token::lexer("3.4");
+
+    assert_eq!(lex.next(), Some(Token::Num(3.4)));
+    assert_eq!(lex.slice(), "3.4");
+}
+
+#[test]
+fn can_parse_numbers() {
+    let mut lex = Token::lexer("3");
+
+    assert_eq!(lex.next(), Some(Token::Num(3.0)));
+    assert_eq!(lex.slice(), "3");
+}
+
+#[test]
+fn can_parse_a_list() {
+    let lex = Token::lexer("(+ 1 2)");
+
+    let tokens: Vec<Token> = lex.collect();
+
+    assert_eq!(
+        tokens,
+        vec![
+            Token::LParen,
+            Token::Plus,
+            Token::Num(1.0),
+            Token::Num(2.0),
+            Token::RParen
+        ]
+    );
+}
+
+
